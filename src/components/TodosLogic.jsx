3d1d6003f1313fe9c0f91503515102ff694db11d
Precious Betine <preciousbetine@gmail.com>
2023-03-27 12:00:16 +0100

    Add edit functionality

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -53,6 +53,16 @@ const TodosLogic = () => {
     setTodos([...todos, newTodo]);
   };
 
+  const setUpdate = (updatedTitle, id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.title = updatedTitle;
+        }
+        return todo;
+      })
+    );
+  };
 
   return (
     <div>
@@ -61,6 +71,7 @@ const TodosLogic = () => {
         todosProps={todos}
         handleChange={handleChange}
         delTodo={delTodo}
+        setUpdate={setUpdate}
       />
     </div>
   )
