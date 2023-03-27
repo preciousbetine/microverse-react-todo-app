ab340a91106844d79a66a90cee781f23fba7b3cd
Precious Betine <preciousbetine@gmail.com>
2023-03-27 11:16:58 +0100

    Create component files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import TodoApp from '@/components/TodoApp';
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TodoApp />
   </React.StrictMode>,
 )
